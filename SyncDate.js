9b889c2a00947ffe6520b2ff762fcc273fafbdd3
Junsik Choi <2318ws@naver.com>
2020-08-08 10:30:12 +0900

    feat : API 추출 후 JSON DB화 : detail API 3천개의 쿼리 날리는 순간 국민재난안전포털 다운됐음(식겁) 2020-08-08 AM

---
+++ SyncDate.js
@@ -0,0 +1,18 @@
+//
+const syncDate = ()=>{
+    let date = new Date();
+    date = new Date(date.setMonth(date.getMonth() - 1));
+    const year = String(date.getFullYear());
+    let last_month = String(date.getMonth() + 1);
+    let cur_month = String(date.getMonth() + 2);
+    let day = String(date.getDate());
+    day = (day.length == 1) ? "0" + day : day;
+    last_month = (last_month.length == 1) ? "0" + last_month : last_month;
+    cur_month = (cur_month.length == 1) ? "0" + cur_month : cur_month;
+    //
+    const search_date_limit = `${year}${last_month}${day}`;
+    const search_end = `${year}${cur_month}${day}`;
+    return {search_date_limit,search_end};
+}
+
+module.exports = syncDate;
