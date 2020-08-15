8be49e0fc65a8ffd557904d0c7ffde6757136054
Junsik Choi <2318ws@naver.com>
2020-08-15 15:47:24 +0900

    Updates

---
+++ precache-manifest.462dfc18625e69175b68bb1b72ee5c40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ddb58477ed822386fdc7faed0ad0e72",
+    "url": "/BDT-front/index.html"
+  },
+  {
+    "revision": "321b543574432ff3a5d9",
+    "url": "/BDT-front/static/css/main.8cdb18a5.chunk.css"
+  },
+  {
+    "revision": "00810bf7dece327fc245",
+    "url": "/BDT-front/static/js/2.0fe9c338.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/BDT-front/static/js/2.0fe9c338.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "321b543574432ff3a5d9",
+    "url": "/BDT-front/static/js/main.22361e38.chunk.js"
+  },
+  {
+    "revision": "7584644eb01933d2f3f7",
+    "url": "/BDT-front/static/js/runtime-main.3c4aaa62.js"
+  }
+]);
