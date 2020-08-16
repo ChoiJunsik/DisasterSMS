ea0c8c2f389b897912f6eff81c807ca2ca8bbe15
Junsik Choi <2318ws@naver.com>
2020-08-16 10:53:10 +0900

    Updates

---
+++ precache-manifest.3e3ff991114d6525acdd9ff6f1974dd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "755bdcd42b188ea283646cb3d461aa83",
+    "url": "/DisasterSMS/index.html"
+  },
+  {
+    "revision": "ae00b281c29ee5a86a86",
+    "url": "/DisasterSMS/static/css/main.7c364a4e.chunk.css"
+  },
+  {
+    "revision": "13165049fde57e20982e",
+    "url": "/DisasterSMS/static/js/2.f8917d84.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/DisasterSMS/static/js/2.f8917d84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae00b281c29ee5a86a86",
+    "url": "/DisasterSMS/static/js/main.bf33b977.chunk.js"
+  },
+  {
+    "revision": "4e8c352fe7fa807ab7dc",
+    "url": "/DisasterSMS/static/js/runtime-main.601fe063.js"
+  }
+]);
